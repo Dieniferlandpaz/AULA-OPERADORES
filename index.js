4df3fce79a84f98756bb6a7f3e49928e87f58c49
Dieniferlandpaz <dienepaz@hotmail.com>
2023-04-12 21:45:24 -0300

    aula de operadores

---
+++ index.js
@@ -0,0 +1,27 @@
+/*(kelvin) = (graus_fahrenheit - 32)* (5/9) + 273.15
+(graus_fahrenheit) = (graus_celsius)*(9/5) + 32*/
+
+/*let kelvin = (77 - 32) * (5/9) + 273.15
+console.log (kelvin + "K")*/
+
+/*B - let graus_fahrenheit = (80)*(9/5) + 32
+console.log (graus_fahrenheit + "°F")*/
+
+/* C - let kelvin = (30- 32)* (5/9) + 273.15
+let graus_fahrenheit = (30)*(9/5) + 32
+console.log ( kelvin + "K" ,  graus_fahrenheit + "°F");*/
+
+/*let graus_celsius = parseFloat(prompt ( "Digite o numero do celcius"));
+kelvin = (graus_celsius - 32) * (5/9) + 273.15
+graus_fahrenheit = (graus_celsius)*(9/5) + 32
+console.log ( graus_fahrenheit + '°F' ,  kelvin + '°K')*/
+
+let quilowatt_hora = 280
+
+
+
+
+
+
+
+
