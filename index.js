56ad3a0afef1266de4c7cb56184a97cd049f2210
Dieniferlandpaz <dienepaz@hotmail.com>
2023-04-13 21:46:35 -0300

    exercios aula de operadores

--- index.js
+++ index.js
@@ -16,7 +16,48 @@ kelvin = (graus_celsius - 32) * (5/9) + 273.15
 graus_fahrenheit = (graus_celsius)*(9/5) + 32
 console.log ( graus_fahrenheit + '°F' ,  kelvin + '°K')*/
 
-let quilowatt_hora = 280
+
+    /* solicitar o nome de usuario, 3 notas e calcular a media.Informar o nome do usuario, a media e se a media for maior que 7 dar os parabens, se nao pedir para repeti8r de ano*/
+/*let nomeDoUsuario = prompt ("Digite o seu nome:");
+let nota1 = Number (prompt ("Digite a priemira nota:"));
+let nota2 = Number ( prompt (" Digite a segunda nota:"));
+let nota3 = Number (prompt ("Digite a terceira nota:"));
+let mediaNota = ( nota1 + nota2 + nota3) /3
+
+console.log ("Seu nome é:  " , nomeDoUsuario + "sua media é: " , mediaNota)
+
+if (mediaNota >= 6 ) { 
+    console.log ("Parabéns você foi aprovado!! ");
+
+} 
+else {
+    console.log ("Você está reprovado :(" );*/
+
+
+
+
+// solicitar um numero para o usuario.
+//validar se é par e informar o resultado
+
+/*let numero = Number (prompt ("Digite um numero: "));
+if (numero % 2===0) {
+    console.log ("seu numero é par!");
+}
+else {
+    console.log ("seu numero é impar!")*/
+
+    // solicitar dois numeros para o usuario exibir o maior//
+    let numero1 = Number(prompt ("Digite o primeiro numero: "));
+    let numero2= Number (prompt ("Digite o segundo numero: "));
+        if (numero1 > numero2 ) {
+            console.log ("Esse numero é o maior.")
+        }
+        else  {
+            console.log ("esse numeor é o maior.")
+        }
+        
+
+
 
 
 
